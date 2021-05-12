8f60cd8e589dca7eb9e72de4fb41a5f5a74d3fd1
Haroun Ach <harounacho21@gmail.com>
2021-05-12 20:51:12 +0100

    Set up handle bar template engine

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.render("home");
 });
 
 app.listen(3000, () => {
