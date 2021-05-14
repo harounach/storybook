515e65dfb21b98e67b4eda34ce3e16cdedb10811
Haroun Ach <harounacho21@gmail.com>
2021-05-14 11:05:32 +0100

    Add DAO objects

--- dao/story.dao.js
+++ dao/story.dao.js
@@ -1,19 +1,31 @@
-exports.addStory = function () {
-  console.log("Story added");
+const Story = require("../models/Story");
+
+exports.addStory = async function (storyObj) {
+  const newStory = new Story(storyObj);
+  return await newStory.save();
 };
 
-exports.editStory = function () {
-  console.log("Story edited");
+exports.editStory = async function (id, updateObj) {
+  return await Story.findOneAndUpdate({ _id: id }, updateObj, {
+    new: true,
+    runValidators: true,
+  });
 };
 
-exports.deleteStory = function () {
-  console.log("Story deleted");
+exports.deleteStory = async function (id) {
+  return await Story.findOneAndRemove({ _id: id });
 };
 
-exports.getAllStories = function () {
-  console.log("All stories");
+exports.getAllStories = async function () {
+  return await Story.find({ status: "public" })
+    .populate("user")
+    .sort({ createdAt: "desc" });
 };
 
 exports.getUserStories = function () {
   console.log("User stories");
+};
+
+exports.getStory = async function (id) {
+  return await Story.findById(id).populate("user");
 };
--- dao/user.dao.js
+++ dao/user.dao.js
@@ -1,3 +1,10 @@
-exports.addUser = function () {
-  console.log("User created");
+const User = require("../models/User");
+
+exports.addUser = async function (userObj) {
+  const newUser = User(userObj);
+  return await newUser.save();
+};
+
+exports.getUser = async function (id) {
+  return await User.findById(id);
 };
